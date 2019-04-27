6745384c5a07a374a5a65a1c2abd0d92b46f6951
Leonardo Santander da Silva <leonardo061297@gmail.com>
2019-04-27 20:10:20 -0300

    AppProtótipoInicial

---
+++ screens/SettingsScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+
+
+export default class SettingsScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Conheça o Projeto',
+
+  };
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+       
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+});
