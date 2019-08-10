0e4be3a95a065a0ac502db1224196a1b3a2f6298
locrz <lucas.oliveira.timburi@gmail.com>
2019-08-10 13:36:59 -0300

    Teclado Subindo

--- acessaOurinhos/screens/InsertPredioScreen.js
+++ acessaOurinhos/screens/InsertPredioScreen.js
@@ -9,6 +9,7 @@ import {
   ScrollView,
   View,
   Modal,
+  KeyboardAvoidingView,
   TouchableOpacity,
   TouchableHighlight,
   StyleSheet
@@ -186,8 +187,8 @@ export default class InsertPredioScreen extends React.Component {
       this.setState({
         teste: pickerSinalizacao
       })
+    }
   }
-}
 
   togglePicker() {
     this.setState({
@@ -205,7 +206,7 @@ export default class InsertPredioScreen extends React.Component {
     const { navigation } = this.props;
     const latitude = navigation.getParam('latitude');
     const longitude = navigation.getParam('longitude');
-    
+
     const pickerProblema = [
       {
         title: 'Escadas e elevadores',
@@ -234,166 +235,165 @@ export default class InsertPredioScreen extends React.Component {
     ];
 
     return (
-      <ScrollView>
-        <SafeAreaView style={{ marginTop: 50 }}>
+      <KeyboardAvoidingView behavior="position" enabled>
+        <ScrollView>
+          <SafeAreaView style={{ marginTop: 50 }}>
 
-          <Text style={styles.welcome}>Prédios Públicos e praças</Text>
-          <Formik
-            initialValues={{ endereco: '', problema: 'Ruas', especificacao: '', detalhe: '', numero: '', bairro: '', cep: '', observacao: '', local: '', latitude: latitude, longitude: longitude, description: '', title: '' }}
-            onSubmit={(values, actions) => {
+            <Text style={styles.welcome}>Prédios Públicos e praças</Text>
+            <Formik
+              initialValues={{ endereco: '', problema: 'Ruas', especificacao: '', detalhe: '', numero: '', bairro: '', cep: '', observacao: '', local: '', latitude: latitude, longitude: longitude, description: '', title: '' }}
+              onSubmit={(values, actions) => {
 
-              var that = this;
+                var that = this;
 
-              that._isMounted = true;
+                that._isMounted = true;
 
-              var rootRef = firebase.database().ref();
+                var rootRef = firebase.database().ref();
 
-              //alert(JSON.stringify(values));
-              var ref = rootRef.child("users");
-              var contador = 0;
+                //alert(JSON.stringify(values));
+                var ref = rootRef.child("users");
+                var contador = 0;
 
-              ref.once("value").then(function (snapshot) {
-                snapshot.forEach(function (childSnapshot) {
-                  contador++;
-                  //console.log("**Contador: ",contador);
+                ref.once("value").then(function (snapshot) {
+                  snapshot.forEach(function (childSnapshot) {
+                    contador++;
+                    //console.log("**Contador: ",contador);
+                  });
+
+                  ref.child(contador).set(values);
                 });
 
-                ref.child(contador).set(values);
-              });
+                setTimeout(() => {
+                  actions.setSubmitting(false);
+                }, 1000);
 
-              setTimeout(() => {
-                actions.setSubmitting(false);
-              }, 1000);
+                Alert.alert("Obrigado!", "Sua reclamação foi adicionada! Agradecemos por colaborar com nossa cidade!");
+                that.props.navigation.navigate('Home');
+              }}
+              validationSchema={validationSchema}
+            >
+              {formikProps => (
+                <React.Fragment>
 
-              Alert.alert("Obrigado!", "Sua reclamação foi adicionada! Agradecemos por colaborar com nossa cidade!");
-              that.props.navigation.navigate('Home');
-            }}
-            validationSchema={validationSchema}
-          >
-            {formikProps => (
-              <React.Fragment>
+                  <View style={{ marginHorizontal: 20, marginVertical: 5 }}>
 
-                <View style={{ marginHorizontal: 20, marginVertical: 5 }}>
+                    <Text>Problema</Text>
+                    <TouchableOpacity style={styles.pickerStyle} onPress={() => this.togglePicker()} >
+                      <Text>{this.state.pickerSelection}</Text>
+                    </TouchableOpacity>
 
-                  <Text>Problema</Text>
-                  <TouchableOpacity style={styles.pickerStyle} onPress={() => this.togglePicker()} >
-                    <Text>{this.state.pickerSelection}</Text>
-                  </TouchableOpacity>
+                    <Modal visible={this.state.pickerDisplayed} animationType={"slide"} transparent={true}>
+                      <View style={{
+                        backgroundColor: '#efefef',
+                        marginBottom: 30,
+                        height: 200,
+                        width: SCREENWIDTH,
+                        bottom: 18,
+                        position: 'absolute'
+                      }}>
+                        <ScrollView style={{ width: SCREENWIDTH, paddingLeft: 10, paddingRight: 10 }}>
+                          <Text style={{
+                            fontSize: 18, fontWeight: 'bold', paddingTop: 10, paddingBottom: 10, alignItems: 'center',
+                            justifyContent: 'center', alignSelf: 'center'
+                          }}>Escolha Um problema</Text>
+                          {pickerProblema.map((value, index) => {
 
-                  <Modal visible={this.state.pickerDisplayed} animationType={"slide"} transparent={true}>
-                    <View style={{
-                      backgroundColor: '#efefef',
-                      marginBottom: 30,
-                      height: 150,
-                      width: SCREENWIDTH,
-                      bottom: 18,
-                      position: 'absolute'
-                    }}>
-                      <ScrollView style={{ width: SCREENWIDTH, paddingLeft: 10, paddingRight: 10 }}>
-                        <Text style={{
-                          fontSize: 18, fontWeight: 'bold', paddingTop: 10, paddingBottom: 10, alignItems: 'center',
-                          justifyContent: 'center', alignSelf: 'center'
-                        }}>Escolha Um problema</Text>
-                        {pickerProblema.map((value, index) => {
+                            return <TouchableHighlight key={index} onPress={() => { this.setPickerValue(value.value); this.mudaPicker(value.value); formikProps.setFieldValue('especificacao', value.value) }} style={{
+                              paddingTop: 4, paddingBottom: 4, alignItems: 'center',
+                              justifyContent: 'center',
+                            }}>
+                              <Text>{value.title}</Text>
+                            </TouchableHighlight>
+                          })}
 
-                          return <TouchableHighlight key={index} onPress={() => { this.setPickerValue(value.value); this.mudaPicker(value.value); formikProps.setFieldValue('especificacao', value.value) }} style={{
-                            paddingTop: 4, paddingBottom: 4, alignItems: 'center',
-                            justifyContent: 'center',
-                          }}>
-                            <Text>{value.title}</Text>
-                          </TouchableHighlight>
-                        })}
-
-
+                        </ScrollView>
                         <TouchableHighlight onPress={() => this.togglePicker()} style={{
                           paddingTop: 4, paddingBottom: 4, alignItems: 'center',
                           justifyContent: 'center',
                         }}>
                           <Text style={{ paddingTop: 10, color: '#999', paddingBottom: 10, fontSize: 18, fontWeight: 'bold' }}>Cancelar</Text>
                         </TouchableHighlight>
-                      </ScrollView>
-                    </View>
-                  </Modal>
-                </View>
+                      </View>
+                    </Modal>
+                  </View>
 
-                <View style={{ marginHorizontal: 20, marginVertical: 5 }}>
+                  <View style={{ marginHorizontal: 20, marginVertical: 5 }}>
 
-                  <Text>Detalhes do Problema</Text>
-                  <TouchableOpacity style={styles.pickerStyle} onPress={() => this.togglePicker2()} >
-                    <Text>{this.state.pickerSelection2}</Text>
-                  </TouchableOpacity>
+                    <Text>Detalhes do Problema</Text>
+                    <TouchableOpacity style={styles.pickerStyle} onPress={() => this.togglePicker2()} >
+                      <Text>{this.state.pickerSelection2}</Text>
+                    </TouchableOpacity>
 
-                  <Modal visible={this.state.pickerDisplayed2} animationType={"slide"} transparent={true}>
-                    <View style={{
-                      backgroundColor: '#efefef',
-                      marginBottom: 30,
-                      height: 150,
-                      width: SCREENWIDTH,
-                      bottom: 18,
-                      position: 'absolute'
-                    }}>
-                      <ScrollView style={{ width: SCREENWIDTH, paddingLeft: 10, paddingRight: 10 }}>
-                        <Text style={{
-                          fontSize: 18, fontWeight: 'bold', paddingTop: 10, paddingBottom: 10, alignItems: 'center',
-                          justifyContent: 'center', alignSelf: 'center'
-                        }}>Escolha Um problema</Text>
-                        {this.state.teste.map((value, index) => {
+                    <Modal visible={this.state.pickerDisplayed2} animationType={"slide"} transparent={true}>
+                      <View style={{
+                        backgroundColor: '#efefef',
+                        marginBottom: 30,
+                        height: 160,
+                        width: SCREENWIDTH,
+                        bottom: 18,
+                        position: 'absolute'
+                      }}>
+                        <ScrollView style={{ width: SCREENWIDTH, paddingLeft: 10, paddingRight: 10 }}>
+                          <Text style={{
+                            fontSize: 18, fontWeight: 'bold', paddingTop: 10, paddingBottom: 10, alignItems: 'center',
+                            justifyContent: 'center', alignSelf: 'center'
+                          }}>Escolha Um problema</Text>
+                          {this.state.teste.map((value, index) => {
 
-                          return <TouchableHighlight key={index} onPress={() => { this.setPickerValue2(value.value); this.mudaPicker(value.value); formikProps.setFieldValue('detalhe', value.value) }} style={{
-                            paddingTop: 4, paddingBottom: 4, alignItems: 'center',
-                            justifyContent: 'center',
-                          }}>
-                            <Text>{value.title}</Text>
-                          </TouchableHighlight>
-                        })}
-
-
+                            return <TouchableHighlight key={index} onPress={() => { this.setPickerValue2(value.value); this.mudaPicker(value.value); formikProps.setFieldValue('detalhe', value.value) }} style={{
+                              paddingTop: 4, paddingBottom: 4, alignItems: 'center',
+                              justifyContent: 'center',
+                            }}>
+                              <Text>{value.title}</Text>
+                            </TouchableHighlight>
+                          })}
+                        </ScrollView>
                         <TouchableHighlight onPress={() => this.togglePicker2()} style={{
                           paddingTop: 4, paddingBottom: 4, alignItems: 'center',
                           justifyContent: 'center',
                         }}>
                           <Text style={{ paddingTop: 10, color: '#999', paddingBottom: 10, fontSize: 18, fontWeight: 'bold' }}>Cancelar</Text>
                         </TouchableHighlight>
-                      </ScrollView>
-                    </View>
-                  </Modal>
-                </View>
+                      </View>
+                    </Modal>
+                  </View>
 
-                <View style={{ marginHorizontal: 20, marginVertical: 5 }}>
-                  <Text style={{ marginBottom: 3 }}>Observações:</Text>
-                  <TextInput
-                    placeholder="descrição do problema"
-                    style={styles.styleForm}
-                    onChangeText={formikProps.handleChange('observacao')}
-                    onBlur={formikProps.handleBlur('observacao')}
-                  />
-                  <Text style={{ color: 'red' }}>
-                    {formikProps.touched.observacao && formikProps.errors.observacao}
-                  </Text>
-                </View>
+                  <View style={{ marginHorizontal: 20, marginVertical: 5 }}>
+                    <Text style={{ marginBottom: 3 }}>Observações:</Text>
+                    <TextInput
+                      placeholder="descrição do problema"
+                      style={styles.styleForm}
+                      onChangeText={formikProps.handleChange('observacao')}
+                      onBlur={formikProps.handleBlur('observacao')}
+                    />
+                    <Text style={{ color: 'red' }}>
+                      {formikProps.touched.observacao && formikProps.errors.observacao}
+                    </Text>
+                  </View>
 
-                <View style={{ marginHorizontal: 20, marginVertical: 5 }}>
-                  <Button
-                    style={{ marginLeft: 50, marginRight: 50 }}
-                    large
-                    icon={{ name: 'camera', type: 'font-awesome' }}
-                    title='Enviar Foto'
-                    onPress={() => this.props.navigation.navigate('Capture')}
-                  />
-                </View>
+                  <View style={{ marginHorizontal: 20, marginVertical: 5 }}>
+                    <Button
+                      style={{ marginLeft: 50, marginRight: 50 }}
+                      large
+                      icon={{ name: 'camera', type: 'font-awesome' }}
+                      title='Enviar Foto'
+                      onPress={() => this.props.navigation.navigate('Capture')}
+                    />
+                  </View>
 
-                {formikProps.isSubmitting ? (
-                  <ActivityIndicator />
-                ) : (
-                    <Button title="Enviar" onPress={formikProps.handleSubmit} />
+                  {formikProps.isSubmitting ? (
+                    <ActivityIndicator />
+                  ) : (
+                      <Button title="Enviar" onPress={formikProps.handleSubmit} />
 
-                  )}
-              </React.Fragment>
-            )}
-          </Formik>
-          <View style={{ height: 30 }}></View>
-        </SafeAreaView>
-      </ScrollView>
+                    )}
+                </React.Fragment>
+              )}
+            </Formik>
+            <View style={{ height: 30 }}></View>
+          </SafeAreaView>
+        </ScrollView>
+      </KeyboardAvoidingView>
     );
   }
 }
